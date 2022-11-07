2825f9a8820066c19ece304b21aad43085c85691
Jiho Lee <optional.int@kakao.com>
2022-11-07 19:52:00 +0900

    chore: nested error routing PoC

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthChecker, CookieChecker } from "@utils";
 import { Account, Posts, Profile, Search, Main } from "@pages";
+import { ErrorPage } from "./pages/Error";
+import Post from "./pages/Posts/Post";
+import ModifyPost from "./pages/Posts/ModifyPost";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<CookieChecker />}>
-        <Route path="account/*" element={<Account />} />
-        <Route element={<AuthChecker />}>
-          <Route path="post/*" element={<Posts />} />
-          <Route path="profile/*" element={<Profile />} />
-          <Route path="search/*" element={<Search />} />
-          <Route path="main/*" element={<Main />} />
+          <Route path="posts/:postId">
+              <Route index element={<Post />} />
+              <Route path="comments" element={<Profile />} />
+              <Route path="modify" element={<ModifyPost />} />
+          </Route>
         </Route>
-      </Route>
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 }
